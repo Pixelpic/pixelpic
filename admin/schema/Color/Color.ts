c4007e279c439c07177a2f08935f03739adb449a
Andrii Maiskyi <andrii.maiskyi@gmail.com>
2022-10-03 21:57:23 +0100

    feat(admin): colors

--- admin/schema/Color/Color.ts
+++ admin/schema/Color/Color.ts
@@ -3,15 +3,15 @@ import { text, integer } from '@keystone-6/core/fields';
 
 export const Color = list({
   fields: {
-    name: text({ validation: { isRequired: true } }),
-    key: text({ validation: { isRequired: true } }),
+    name: text({ validation: { isRequired: true }, isIndexed: 'unique' }),
     red: integer({ validation: { isRequired: true } }),
     green: integer({ validation: { isRequired: true } }),
     blue: integer({ validation: { isRequired: true } }),
+    hex: text({ validation: { isRequired: true }, isIndexed: 'unique' }),
   },
   ui: {
     listView: {
-      initialColumns: ['name'],
+      initialColumns: ['name', 'hex'],
     },
   },
 });
