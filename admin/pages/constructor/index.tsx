aa226289351214ae7f5c0d399b613f8a438891d4
Andrii Maiskyi <andrii.maiskyi@gmail.com>
2022-10-06 00:34:34 +0100

    feat(admin): constructor

--- admin/pages/constructor/index.tsx
+++ admin/pages/constructor/index.tsx
@@ -1,9 +1,29 @@
-/** @jsxRuntime classic */
-/** @jsx jsx */
-import { jsx } from '@keystone-ui/core';
-import { PageContainer } from '@keystone-6/core/admin-ui/components';
+import React, { useState } from 'react';
 import { Heading } from '@keystone-ui/core';
+import { Step, StepLabel } from '@mui/material';
+import { Root, Container, Stepper, Content } from './Constructor.style';
+import { CONSTRUCTOR_STEPS } from './Constructor.const';
+import { ConstructorStepId } from './Constructor.types';
 
 export default function Constructor() {
-  return <PageContainer header={<Heading type="h3">Constructor</Heading>}>123</PageContainer>;
+  const [step, setStep] = useState<ConstructorStepId>(ConstructorStepId.FILE);
+
+  const activeStep = CONSTRUCTOR_STEPS.findIndex(({ id }) => id === step);
+
+  return (
+    <Root header={<Heading type="h3">Constructor</Heading>}>
+      <Container>
+        <Stepper nonLinear activeStep={activeStep}>
+          {CONSTRUCTOR_STEPS.map(({ label, id }) => {
+            return (
+              <Step key={id}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        <Content></Content>
+      </Container>
+    </Root>
+  );
 }
