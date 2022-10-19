27ad462376e2a6a7fbce29c478830169eb70618f
Andrii Maiskyi <andrii.maiskyi@gmail.com>
2022-10-19 22:59:18 +0100

    feat(admin): user manual

--- admin/api/sale/doc/user-manual/get/index.ts
+++ admin/api/sale/doc/user-manual/get/index.ts
@@ -30,26 +30,47 @@ export const getController = async (req: Request<RequestParams>, res: Response) 
 
     const { matrix, base64 } = await loadImage(sale.image?.image?.publicUrl);
 
-    const { horizontal = 0, vertical = 0 } = sale.frame || {};
+    const { horizontal: h = 0, vertical: v = 0 } = sale.frame || {};
 
     const doc = new jsPDF({
       format: 'a5',
       unit: 'px',
     });
 
+    const horizontal = h || 0;
+    const vertical = v || 0;
     const baseOffsetX = (doc.internal.pageSize.getWidth() - BRICK_SIZE * BRICK_COUNT) / 2;
     const baseOffsetY = (doc.internal.pageSize.getWidth() - BRICK_SIZE * BRICK_COUNT) / 2;
     const previewOffsetX = baseOffsetX / 2;
     const previewOffsetY = baseOffsetY / 2;
-    const previewHeight = 100;
-    const previewWidth = vertical && horizontal ? (previewHeight / vertical) * horizontal : 0;
+    const previewHeight = 120;
+    const previewWidth = (previewHeight / vertical) * horizontal;
     const frameOffsetX = baseOffsetX;
     const frameOffsetY = previewOffsetY + previewHeight + baseOffsetY;
 
-    for (let y = 0; y < (vertical || 0); y = y + 1) {
-      for (let x = 0; x < (horizontal || 0); x = x + 1) {
+    for (let y = 0; y < vertical; y = y + 1) {
+      for (let x = 0; x < horizontal; x = x + 1) {
         doc.addPage();
         doc.addImage(base64, 'PNG', previewOffsetX, previewOffsetY, previewWidth, previewHeight);
+        for (
+          let j = 0, posY = previewOffsetY;
+          j < vertical;
+          j++, posY += previewHeight / vertical
+        ) {
+          for (
+            let i = 0, posX = previewOffsetX;
+            i < horizontal;
+            i++, posX += previewWidth / horizontal
+          ) {
+            const opacity = y === j && x === i ? 0 : 0.7;
+            doc.setDrawColor(0);
+            doc.saveGraphicsState();
+            doc.setGState(doc.GState({ opacity }));
+            doc.setFillColor(255, 255, 255);
+            doc.rect(posX, posY, previewWidth / horizontal, previewHeight / vertical, 'F');
+            doc.restoreGraphicsState();
+          }
+        }
         for (
           let i = x * BRICK_COUNT, posX = frameOffsetX;
           i < x * BRICK_COUNT + BRICK_COUNT;
