90d83bc47ce0fa70e112cc8a760d3defc9421bf7
Andrii Maiskyi <andrii.maiskyi@gmail.com>
2023-10-25 22:08:39 +0100

    fix: colors

--- admin/configs/env.config.ts
+++ admin/configs/env.config.ts
@@ -1,4 +1,4 @@
-const DEV_DATABASE_URL = 'postgres://am:password@localhost:5432/pixelpic-db';
+const DEV_DATABASE_URL = 'postgresql://user:password@localhost:5432/pixelpic?schema=public';
 const DEV_SESSION_SECRET = '-- DEV COOKIE SECRET; CHANGE ME --';
 
 export const DATABASE_URL = process.env.DATABASE_URL || DEV_DATABASE_URL;
